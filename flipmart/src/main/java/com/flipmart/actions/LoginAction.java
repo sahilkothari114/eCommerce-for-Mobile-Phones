911cec71f058a494c2de0e52e865c37651608ff2
mipalak <mipalak96@gmail.com>
2018-06-04 23:16:02 +0530

    login added

---
+++ flipmart/src/main/java/com/flipmart/actions/LoginAction.java
@@ -0,0 +1,28 @@
+package com.flipmart.actions;
+
+import com.flipmart.dao.Admin;
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Result;
+
+@Action(value = "Login", results = {@Result(name = "success" , location = "/View/login.jsp")})
+public class LoginAction extends ActionSupport {
+    
+    private Admin adm;
+    
+    @Override
+    public String execute() {
+        this.adm = new Admin("Palak","Mittal","admin@gmail.com","root",395007,"Vesu","9714773181","hiii picture");
+        return SUCCESS;
+    }
+
+    public Admin getAdm() {
+        return adm;
+    }
+
+    public void setAdm(Admin adm) {
+        this.adm = adm;
+    }
+
+    
+}
