5245599f7d5356d6ad1ace206f12ff44eff5fc0f
Himanshu Sahu <anshh31@gmail.com>
2018-07-30 18:35:19 +0530

    Admin Profile - GET, PUT Request, Validation and Rendering using Dummy Data

--- flipmart-webapp/web/src/main/webapp/assets/controllers/dashboard.controller.js
+++ flipmart-webapp/web/src/main/webapp/assets/controllers/dashboard.controller.js
@@ -4,13 +4,12 @@ angular.module("dashboardApp", []).controller("StatisticsCtrl", function($scope,
         $scope.monthlySales = response.data.monthlySales;
         $scope.noOfUsers = response.data.noOfUsers;
         $scope.noOfProducts = response.data.noOfProducts;
-    }
+    };
     var onPageLoadError = function(response) {
         //Display Error Message or route to server not available page
-    }
+    };
     function pageLoad() {
         $http.get("../../assets/controllers/DummyData/dashboardPageLoad.json").then(onPageLoadSuccess, onPageLoadError);
     }
-
     pageLoad();
 });
